32b63fe1fd3ac8cef7e16d926b0af9417e9b4b1e
Andreas Larsen <andreas@northcode.no>
2014-03-25 23:26:53 +0100

    worked more on paging

--- nvm2/virtual-machine/Pager.cs
+++ nvm2/virtual-machine/Pager.cs
@@ -12,6 +12,8 @@ namespace nvm2
 
 	public class Pager
 	{
+		public const int PAGE_TABLE_SIZE = 512;
+
 		vm machine;
 		Memory ram;
 
@@ -27,6 +29,64 @@ namespace nvm2
 				PageDirectory[i] = new PageDirectoryEntry() { PTAddress = 0, AccessLevel = false, InUse = false };
 			}
 		}
+
+		public int CreatePageEntry (bool mode)
+		{
+			//Declare stuff
+			int index = 0;
+			PageDirectoryEntry entry = new PageDirectoryEntry();
+			//Find a free ram frame for the first page
+			Frame mainFrame = ram.findFreeFrame();
+			mainFrame.IsFree = false;
+			entry.PTAddress = mainFrame.Address;
+			entry.AccessLevel = mode;
+			entry.InUse = true;
+			//Find free entry in page directory
+			for (int i = 0; i < PageDirectory.Length; i++) {
+				if (!PageDirectory[i].InUse) {
+					PageDirectory[i] = entry;
+					index = i;
+					break;
+				}
+			}
+			//Write page table to ram for entry
+			WritePageTable(entry);
+			return index;
+		}
+
+		public void FreePageEntry (int index)
+		{
+			//Get entry
+			PageDirectoryEntry entry = PageDirectory[index];
+
+			FreePageTable(entry);
+		}
+
+		public void FreePageTable (PageDirectoryEntry entry)
+		{
+			uint addr = entry.PTAddress;
+
+			//Read pages
+			for (int i = 0; i < PAGE_TABLE_SIZE; i++) {
+				uint frameAddr = ram.ReadUInt((uint)(addr + (i*4)));
+				int frameindex = (int)Math.Floor((float)(frameAddr / Frame.FRAME_SIZE));
+				Frame frame = ram.getFrame(frameindex);
+				frame.IsFree = true; // Set frame to free, clears the frame
+			}
+		}
+
+		public void WritePageTable (PageDirectoryEntry entry)
+		{
+			//Write page table
+			uint addr = entry.PTAddress; // Get page table address
+			// Write: Page addresses
+			for (int i = 0; i < PAGE_TABLE_SIZE; i++) {
+				ram.Write((uint)(addr + (i*4)), 0);
+			}
+			//Write first page address start
+			ram.Write(addr,addr + 512);
+		}
+
 	}
 }
 
