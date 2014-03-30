b751cae056bbcf6a93a20222e936dba62fd131d4
Andreas Larsen <andreas@northcode.no>
2014-03-30 17:04:07 +0200

    started on malloc

--- nvm2/virtual-machine/Pager.cs
+++ nvm2/virtual-machine/Pager.cs
@@ -10,6 +10,7 @@ namespace nvm2
 		public bool InUse;
 		public uint stack_pointer;
 		public uint heap_pointer;
+		public uint free_pointer;
 	}
 
 	public class Pager
@@ -124,18 +125,7 @@ namespace nvm2
 			return (uint)(pageaddr + offset);
 		}
 
-		public void SetupMemory(PageDirectoryEntry entry, uint stack_pointer, uint heap_pointer) {
-			entry.stack_pointer = stack_pointer;
-			entry.heap_pointer = heap_pointer;
-		}
 
-		public void SetupMemoryAllocation(PageDirectoryEntry entry) {
-			// Get address
-			//uint addr = entry.PTAddress + PAGE_TABLE_SIZE;
-			// Write free pointer
-			ram.Write(entry.heap_pointer,entry.heap_pointer);
-			ram.Write(entry.heap_pointer + 4,GetPageDirectoryEntrySize(entry));
-		}
 
 		public void AddPage (PageDirectoryEntry entry)
 		{
@@ -149,7 +139,7 @@ namespace nvm2
 					break;
 				}
 			}
-		}		
+		}
 
 		public void DumpPageTable (PageDirectoryEntry entry)
 		{
@@ -161,6 +151,73 @@ namespace nvm2
 			}
 		}
 
+
+
+		// ---- MEMORY ALLOC STUFF ---- all memory allocation is done with relative addresses
+
+		/// <summary>
+		/// Setup the heap and stack for the page table
+		/// </summary>
+		/// <param name='entry'>
+		/// Page directory entry
+		/// </param>
+		/// <param name='stack_pointer'>
+		/// New stack_pointer, relative address.
+		/// </param>
+		/// <param name='heap_pointer'>
+		/// New heap_pointer, relative address
+		/// </param>
+		public void SetupMemory(PageDirectoryEntry entry, uint stack_pointer, uint heap_pointer) {
+			entry.stack_pointer = stack_pointer;
+			entry.heap_pointer = heap_pointer;
+		}
+
+		/// <summary>
+		/// Setup the heap memory allocation for the page table
+		/// </summary>
+		/// <param name='entry'>
+		/// Page table to setup
+		/// </param>
+		public void SetupMemoryAllocation(PageDirectoryEntry entry) {
+			entry.free_pointer = entry.heap_pointer; //set free-list pointer to start of heap
+			ram.Write(TranslateVitrualAddress(entry.heap_pointer,entry),0); // write 0 because there is no next block
+			ram.Write(TranslateVitrualAddress(entry.heap_pointer,entry) + 4,(uint)(GetPageDirectoryEntrySize(entry) - entry.heap_pointer)); //Write size of free block (total size of heap)
+		}
+
+		/// <summary>
+		/// Allocate a new block of memory in the page table with the specified size
+		/// </summary>
+		/// <param name='size'>
+		/// Size of memory to allocate
+		/// </param>
+		/// <param name='entry'>
+		/// Page table to use
+		/// </param>
+		/// <returns>
+		/// The address of the allocated block
+		/// </returns>
+		public uint Malloc(uint size,PageDirectoryEntry entry) {
+			uint lastaddr = entry.free_pointer; //last free block pointer
+			//loop through freelist , check for large enough chunk
+			for (uint addr = entry.free_pointer; addr < GetPageDirectoryEntrySize(entry); ) {
+				uint nextblock = ram.ReadUInt(TranslateVitrualAddress(addr, entry)); //read address of next free block
+				uint blocksize = ram.ReadUInt(TranslateVitrualAddress(addr + 4, entry)); //read size of current free block
+				if (blocksize > size) {
+					uint newsize = blocksize - size; //get new size of free block
+					uint newaddr = addr + size; //get new address of free block
+					if (newsize > 0) { //if size == 0, block is gone
+						ram.Write(TranslateVitrualAddress(newaddr,entry),nextblock); // set next block pointer
+						ram.Write(TranslateVitrualAddress(newaddr + 4,entry),newsize); //write new size of new freeblock
+						ram.Write(TranslateVitrualAddress(lastaddr,entry),TranslateVitrualAddress(newaddr,entry)); //set pointer of last block to point to this new one
+					}
+					else
+					{
+						ram.Write(TranslateVitrualAddress(lastaddr,entry),TranslateVitrualAddress(nextblock,entry)); //set pointer to skip unexisting block
+					}
+					return addr; //return address of allocated space
+				}
+			}
+		}
 	}
 }
 
